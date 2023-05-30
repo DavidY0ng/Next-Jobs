04aefb4ed7af4810b9112136505819883f57abc4
Yong Chee Ho <yongcheeho@gmail.com>
2023-05-30 10:39:49 +0800

    Redirection for /edit checking userID now works

--- src/routes/jobs/[slug]/edit/+page.js
+++ src/routes/jobs/[slug]/edit/+page.js
@@ -5,17 +5,19 @@ import {goto} from '$app/navigation';
 
 export async function load({ fetch, params}) {
   
-  if (!getUserId()) {
-    goto (`/jobs/${params.slug}`)
-  }
-  
   const resp = await fetch(PUBLIC_BACKEND_BASE_URL + `/api/collections/jobs/records/${params.slug}`);
-
   const res = await resp.json();
+
+  if (getUserId() != res.user) {
+    goto (`/jobs/${params.slug}`)
+  } 
+  
   if (resp.status == 200) {
     return {
       job: res
+       
     }
+  
   } else {
     return {
       jobs: []
